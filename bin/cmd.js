c331a4e15a1f6328fe4f34745bc4bd8ae8fe9705
harish-chandra-89 <54844316+harish-chandra-89@users.noreply.github.com>
2020-01-06 22:09:15 +0530

    feat: update outdated packages (#24)

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,9 +1,9 @@
 #! /usr/bin/env node
 const opts = require('../options')
 
-if (process.version.match(/v(\d+)\./)[1] < 6) {
+if (process.version.match(/v(\d+)\./)[1] < 8) {
   // eslint-disable-next-line no-console
-  console.error('standard: Node 6 or greater is required. `standard` did not run.')
+  console.error('standard: Node 8 or greater is required. `standard` did not run.')
 } else {
   require('standard-engine').cli(opts)
 }
