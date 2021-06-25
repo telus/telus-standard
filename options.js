29940de708592b9205749288e50c800ab41d10bd
Nitin Agrawal <nitin.agrawal1@telusinternational.com>
2021-06-25 17:10:18 +0530

    fix: config and depedencies

--- options.js
+++ options.js
@@ -1,16 +1,17 @@
 const eslint = require('eslint')
+const path = require('path')
 const pkg = require('./package.json')
 
 module.exports = {
+  version: pkg.version,
+  homepage: pkg.homepage,
   bugs: pkg.bugs.url,
-  cmd: 'telus-standard',
   eslint,
+  cmd: 'telus-standard',
+  tagline: 'Use TELUS JavaScript Standard Style',
   eslintConfig: {
-    baseConfig: require('./eslintrc'),
+    configFile: path.join(__dirname, './eslintrc.js'),
     useEslintrc: true,
     cache: false
-  },
-  homepage: pkg.homepage,
-  tagline: 'Use TELUS JavaScript Standard Style',
-  version: pkg.version
+  }
 }
