08575419d56e4914d04c03ed8eadab31f55f677b
afzal-telus <62990777+afzal-telus@users.noreply.github.com>
2021-06-29 12:08:39 +0530

    Sec vul fix (#66)
    * fix: config and depedencies
    
    * fix: updated husky config
    
    * feat: added support of github action v2
    
    * fix: security vulnerabilites and depedencies
    
    * fix:  git action v2 text updated
    
    * fix: audit ci config
    
    * fix: lint issue
    
    Co-authored-by: Nitin Agrawal <nitin.agrawal1@telusinternational.com>

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,9 +1,5 @@
-#! /usr/bin/env node
-const opts = require('../options')
+#!/usr/bin/env node
 
-if (process.version.match(/v(\d+)\./)[1] < 8) {
-  // eslint-disable-next-line no-console
-  console.error('standard: Node 8 or greater is required. `standard` did not run.')
-} else {
-  require('standard-engine').cli(opts)
-}
+const opts = require('../options')
+require('standard-engine').cli(opts)
+
